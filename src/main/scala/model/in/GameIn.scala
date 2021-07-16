86964bbdd55473f919da26c590c391a620f8aa0b
NareshDRoger <nareshrogeraly@gmail.com>
2021-07-16 22:18:51 +0200

    update

---
+++ src/main/scala/model/in/GameIn.scala
@@ -0,0 +1,12 @@
+package model.in
+
+case class GameIn (
+//                        box_art_url: String,
+//                        name: String,
+                        name: String)
+
+
+
+
+
+
---
+++ src/main/scala/model/out/GameOut.scala
@@ -0,0 +1,5 @@
+package model.out
+
+class GameOut {
+
+}
